4ff2a77bcde7180be59632199b4dc61450ff13d9
Shiv108 <shivy101@live.com>
2020-08-24 23:30:37 +0200

    LP

---
+++ src/components/Quote/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import './style.css'
+
+const Quote = ()=>{
+    
+    return(
+        <div>Hello</div>
+    )
+}
+export default Quote;
---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+
+import "./style.css";
+import Quote from '../../components/Quote';
+
+const App = () => {
+  
+  return (
+    <div className="mainContainer">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=x2D7jHfitzk"
+        width="100%"
+        height="100%"
+        style={{opacity:0.65}}
+        playing={true}
+        loop={true}
+        muted={true}
+      />
+      <div className="mainContent">
+        <h1 className="mainTitle">Title</h1>
+        <h2>subtitle</h2>
+        <button>Enter</button>
+        <Quote/>
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
