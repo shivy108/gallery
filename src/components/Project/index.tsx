b9bb278ff7cc0ae8dbe12890827f6ec8f71b93bd
Shivesh Dindayal <shivy101@live.com>
2020-09-06 17:26:11 +0200

    project

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,19 +1,36 @@
 import React, { FunctionComponent } from "react";
-import "./style.css";
+import "./style.scss";
 
 type Props = {
   title: String;
   technologies: String;
   link: string;
+  image1: string;
+  image2: string;
 };
 
-const Project: FunctionComponent<Props> = ({ title, technologies, link }) => {
+const Project: FunctionComponent<Props> = ({
+  title,
+  technologies,
+  link,
+  image1,
+  image2,
+}) => {
   return (
-    <div className="projectContainer">
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <h1 className="ProjectTitle">{title}</h1>
+    <div className='projectContainer'>
+      <a
+        className='ProjectTitle'
+        href={link}
+        target='_blank'
+        rel='noopener noreferrer'>
+        <h1>{title}</h1>
       </a>
-      <p className="ProjectTech">{technologies}</p>
+      <div className='imageWrapper'>
+        <img className='image1' src={image1} alt={image1} />
+        <img src={image2} alt={image2} className='image2' />
+        <p className='ProjectTech'>{technologies}</p>
+      </div>
+      
     </div>
   );
 };
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -4,6 +4,10 @@ import "./style.scss";
 import { FaChevronCircleDown } from "react-icons/fa";
 import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
 import { AiOutlineMail, AiOutlineGitlab } from "react-icons/ai";
+import netflix from "../../assets/netflix.jpg";
+import netflix2 from "../../assets/netflix2.webp";
+import Project from "../../components/Project";
+
 
 const App = () => {
   const [show, handleShow] = useState(false);
@@ -23,66 +27,63 @@ const App = () => {
   }, [show]);
 
   return (
-    <div className="page">
+    <div className='page'>
       <div className={show ? "navBlur" : "nav"}>
-        <div className="navIconWrapper">
-          <div className="iconWrapper1">
-            <div className="iconWrapper2">
-              <h4 className="navIcon">SD</h4>
+        <div className='navIconWrapper'>
+          <div className='iconWrapper1'>
+            <div className='iconWrapper2'>
+              <h4 className='navIcon'>SD</h4>
             </div>
           </div>
         </div>
-        <div className="navMenu">
-          <h4 className="navItem">Home</h4>
-          <h4 className="navItem">About</h4>
-          <h4 className="navItem">Portfolio</h4>
-          <div className="navButtonWrapper">
-            <h4 className="navButton">Resume</h4>
+        <div className='navMenu'>
+          <h4 className='navItem'>Home</h4>
+          <h4 className='navItem'>About</h4>
+          <h4 className='navItem'>Portfolio</h4>
+          <div className='navButtonWrapper'>
+            <h4 className='navButton'>Resume</h4>
           </div>
         </div>
       </div>
-      <div className="welcomeBlock">
-        <FaChevronCircleDown className="icon" />
+      <div className='welcomeBlock'>
+        <FaChevronCircleDown className='icon' />
       </div>
-      <div className="welcomeMessage">
-        <h2 className="message">
-          Hello, <h2 className="messageName">I am Shivesh</h2>
-          <h2 className="messageName">I build things for the web</h2>
+      <div className='welcomeMessage'>
+        <h2 className='message'>
+          Hello, <h2 className='messageName'>I am Shivesh</h2>
+          <h2 className='messageName'>I build things for the web</h2>
         </h2>
       </div>
-      <div className="socialBar">
-        <div className="socialWrapper">
+      <div className='socialBar'>
+        <div className='socialWrapper'>
           <a
-            className="socialIcon"
-            href="https://github.com/shivy108"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='socialIcon'
+            href='https://github.com/shivy108'
+            target='_blank'
+            rel='noopener noreferrer'>
             <IoLogoGithub />
           </a>
           <a
-            href="https://www.linkedin.com/in/shivesh-dindayal-25291a111/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IoLogoLinkedin className="socialIcon" />
+            href='https://www.linkedin.com/in/shivesh-dindayal-25291a111/'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <IoLogoLinkedin className='socialIcon' />
           </a>
-          <a href="mailto:shiveshdindayal@gmail.com">
-            <AiOutlineMail className="socialIcon" />
+          <a href='mailto:shiveshdindayal@gmail.com'>
+            <AiOutlineMail className='socialIcon' />
           </a>
           <a
-            href="https://gitlab.propulsion-home.ch/Shiv108"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AiOutlineGitlab className="socialIcon" />
+            href='https://gitlab.propulsion-home.ch/Shiv108'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <AiOutlineGitlab className='socialIcon' />
           </a>
         </div>
       </div>
-      <section className="about">
-        <div className="aboutWrapper">
-          <h1 className="aboutTitle">About me</h1>
-          <p className="aboutPara">
+      <section className='about'>
+        <div className='wrapper'>
+          <h1 className='title'>About me</h1>
+          <p className='aboutPara'>
             I am Shivesh, a software engineer based in Zurich. I enjoy creating
             cool and clever interactive applications with React, Angular,
             Typescript, JavaScript, Python, Django and Docker. I am always up
@@ -90,9 +91,10 @@ const App = () => {
           </p>
         </div>
       </section>
-      <section className="projects">
-        <div className="projectWrapper">
-          <h1 className="aboutTitle">My Projects</h1>
+      <section className='projects'>
+        <div className='wrapper'>
+          <h1 className='title'>My Projects</h1>
+          <Project title="Netflix Clone" link="https://netflix-clone130678.web.app/" image1={netflix} image2={netflix2} technologies="React, styled-components, firebase, typeScript"/>
         </div>
       </section>
     </div>
