3897d46947ca03025ca7d42809597a75ea7596cb
Shivesh Dindayal <shivy101@live.com>
2020-09-06 10:45:52 +0200

    socialBar

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-
 import "./style.scss";
-import { FaChevronCircleDown } from 'react-icons/fa';
+import { FaChevronCircleDown } from "react-icons/fa";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+import { AiOutlineMail, AiOutlineGitlab } from "react-icons/ai";
 
 const App = () => {
   const [show, handleShow] = useState(false);
@@ -20,6 +21,7 @@ const App = () => {
       });
     };
   }, [show]);
+
   return (
     <div className='page'>
       <div className={show ? "navBlur" : "nav"}>
@@ -40,13 +42,21 @@ const App = () => {
         </div>
       </div>
       <div className='welcomeBlock'>
-        <FaChevronCircleDown className="icon"/>
+        <FaChevronCircleDown className='icon' />
       </div>
       <div className='welcomeMessage'>
         <h2 className='message'>
           Hello, <h2 className='messageName'>I am Shivesh</h2>
-          <h2 className="messageName">I build things for the web</h2>
+          <h2 className='messageName'>I build things for the web</h2>
         </h2>
+      </div>
+      <div className='socialBar'>
+        <div className='socialWrapper'>
+          <a className='socialIcon' href="https://github.com/shivy108" target="_blank" rel="noopener noreferrer"><IoLogoGithub  /></a>
+          <a href="https://www.linkedin.com/in/shivesh-dindayal-25291a111/" target="_blank" rel="noopener noreferrer"><IoLogoLinkedin className='socialIcon' /></a>
+          <a href="mailto:shiveshdindayal@gmail.com"><AiOutlineMail className='socialIcon' /></a>
+          <a href="https://gitlab.propulsion-home.ch/Shiv108" target="_blank" rel="noopener noreferrer"><AiOutlineGitlab className='socialIcon' /></a>
+        </div>
       </div>
     </div>
   );
