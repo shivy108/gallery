6cdc83b5b28b5563f5ecf0985de8067ae2a7c27a
Shivesh Dindayal <shivy101@live.com>
2020-09-13 17:43:55 +0200

    styling animation

--- src/components/DisplayProject/index.tsx
+++ src/components/DisplayProject/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import {IoIosArrowDroprightCircle} from 'react-icons/io'
-import {IoIosArrowDropleftCircle} from 'react-icons/io'
-import './style.scss'
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import "./style.scss";
 import Project from "../Project";
 import netflix from "../../assets/netflix.jpg";
 import laptop from "../../assets/laptop.png";
@@ -16,7 +16,6 @@ const DisplayProject = () => {
     if (one) {
       setOne(false);
       setTwo(true);
-     
     }
     if (two) {
       setTwo(false);
@@ -44,15 +43,18 @@ const DisplayProject = () => {
   };
 
   return (
-    <div className="DisplayProject">
-      <IoIosArrowDroprightCircle className="right" onClick={handleRight}/>
-        
-      <IoIosArrowDropleftCircle className="left" onClick={handleLeft}/>
-        
+    <div className='DisplayProject'>
+      <div className='RightWrapper'>
+        <IoIosArrowDroprightCircle className='right' onClick={handleRight} />
+      </div>
+      <div className='LeftWrapper'>
+        <IoIosArrowDropleftCircle className='left' onClick={handleLeft} />
+      </div>
+
       {one ? (
         <Project
-          title="CV Generator"
-          link="https://drive.google.com/file/d/1NmoW0M0gn1n19iPn2Hk7UMuC08_aPFf2/view?ts=5f31473b"
+          title='CV Generator'
+          link='https://drive.google.com/file/d/1NmoW0M0gn1n19iPn2Hk7UMuC08_aPFf2/view?ts=5f31473b'
           image={laptop}
           technologies={[
             "react",
@@ -69,8 +71,8 @@ const DisplayProject = () => {
 
       {two ? (
         <Project
-          title="Netflix Clone"
-          link="https://netflix-clone130678.web.app/"
+          title='Netflix Clone'
+          link='https://netflix-clone130678.web.app/'
           image={netflix}
           technologies={[
             "React",
@@ -87,8 +89,8 @@ const DisplayProject = () => {
       )}
       {three ? (
         <Project
-          title="Online Shop"
-          link="https://netflix-clone130678.web.app/"
+          title='Online Shop'
+          link='https://netflix-clone130678.web.app/'
           image={nike}
           technologies={["React", "Sass", "firebase", "typeScript", "Redux"]}
         />
